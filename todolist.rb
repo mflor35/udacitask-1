63eab48b4fa2ead613b04e8734d1ac0aef07224b
Miguel Flores Silverio <floresmigu3l@gmail.com>
2016-02-16 15:58:59 -0800

    remove _started from files

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,12 @@
 class TodoList
     # methods and stuff go here
+    def add_item(item)
+
+    end
 end
 
 class Item
     # methods and stuff go here
+    def is_complete?
+      return comple?
 end
