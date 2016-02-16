1c215aef5dd866cede14d7d8e8fbd1e4a6229edc
Miguel Flores Silverio <floresmigu3l@gmail.com>
2016-02-16 15:46:53 -0800

    Possible methods

--- udacitask_starter.rb
+++ udacitask_starter.rb
@@ -1,23 +1,26 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
-
+my_todo = todolist.new
 # Add four new items
-
+my_todo.add_item()
+my_todo.add_item()
+my_todo.add_item()
 # Print the list
-
+my_todo.show()
 # Delete the first item
-
+my_todo.delete_item()
 # Print the list
-
+my_todo.show()
 # Delete the second item
-
+my_todo.delete_item()
 # Print the list
-
+my_todo.show()
 # Update the completion status of the first item to complete
-
+my_todo.mark_complete()
 # Print the list
-
+my_todo.show()
 # Update the title of the list
-
+my_todo.set_title()
 # Print the list
+my_todo.show()
