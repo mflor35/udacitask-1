06980003a8d07c89cb3f84ebf25d0748a2358a5a
Miguel Flores Silverio <floresmigu3l@gmail.com>
2016-02-16 17:20:13 -0800

    Add tasks to the list and show method to display current items in the list.

--- todolist.rb
+++ todolist.rb
@@ -1,12 +1,64 @@
 class TodoList
     # methods and stuff go here
-    def add_item(item)
+    attr_reader :title, :items
+    #initilize todo list with a title and not items
+    def initialize(list_title)
+      @title = list_title
+      @items = Array.new # Starts empty! No items yet!
+    end
 
+    # Creates a new Item and adds it to the array of Items
+    def add_item(new_item)
+      item = Item.new(new_item)
+      item.id += 1
+      @items.push(item)
+    end
+
+    def title
+      @title
+    end
+
+    def items
+      @items
+    end
+
+    def set_title(new_title)
+      @title = new_title
+    end
+
+    def show
+
+      puts @title
+      for i in @title.length
+        print "-"
+      end
+      @items.each do |item|
+        puts "#{item.item_id} - #{item.get_description} %-3s Completed:#{item.completed?}"
+
+      end
     end
 end
 
 class Item
     # methods and stuff go here
-    def is_complete?
-      return comple?
+    attr_reader :description,:completed_status :id
+
+    # Initilize item with a description
+    def initialize(item_description)
+      @description = item_description
+      @completed_status = false
+      @id = 1
+    end
+
+    def completed?
+      @completed_status
+    end
+
+    def get_description
+      @description
+    end
+
+    def item_id
+      @id
+    end
 end
--- udacitask.rb
+++ udacitask.rb
@@ -3,24 +3,24 @@ require_relative 'todolist.rb'
 # Creates a new todo list
 my_todo = todolist.new
 # Add four new items
-my_todo.add_item()
-my_todo.add_item()
-my_todo.add_item()
+my_todo.add_item("Task 1")
+my_todo.add_item("Task 2")
+my_todo.add_item("Task 3")
 # Print the list
-my_todo.show()
+my_todo.show
 # Delete the first item
-my_todo.delete_item()
+#my_todo.delete_item()
 # Print the list
-my_todo.show()
+#my_todo.show()
 # Delete the second item
-my_todo.delete_item()
+#my_todo.delete_item()
 # Print the list
-my_todo.show()
+#my_todo.show()
 # Update the completion status of the first item to complete
-my_todo.mark_complete()
+#my_todo.mark_complete()
 # Print the list
-my_todo.show()
+#my_todo.show()
 # Update the title of the list
-my_todo.set_title()
+#my_todo.set_title()
 # Print the list
-my_todo.show()
+#my_todo.show()
