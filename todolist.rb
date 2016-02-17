6e8a04648c583311380b12f76563e923e2432de3
Miguel Flores Silverio <floresmigu3l@gmail.com>
2016-02-16 18:50:07 -0800

    Methods needed to pass the tests on udasitask.rb

--- todolist.rb
+++ todolist.rb
@@ -10,55 +10,86 @@ class TodoList
     # Creates a new Item and adds it to the array of Items
     def add_item(new_item)
       item = Item.new(new_item)
-      item.id += 1
       @items.push(item)
     end
 
+    # Displays the title of the list
     def title
       @title
     end
 
+    # Returns the array containing items in the list
     def items
       @items
     end
 
+    # new_title is set as the title of the todolist
     def set_title(new_title)
       @title = new_title
     end
 
+    # Displays the items in the list in a nice way
     def show
-
       puts @title
-      for i in @title.length
+      for i in 0..@title.length
         print "-"
       end
+      puts
       @items.each do |item|
-        puts "#{item.item_id} - #{item.get_description} %-3s Completed:#{item.completed?}"
+        puts "#{item.get_id}- #{item.get_description} %-5s"%"Completed:#{item.completed?}"
+      end
+      puts
+    end
 
+    # Finds the item that matches item_description
+    def find_item(item_description)
+      @items.each do |item|
+        if item_description == item.get_description
+          return item
+        end
       end
     end
+
+    # Deletes the item that matches the item_description
+    def delete_item(item_description)
+      @items.delete(find_item(item_description))
+    end
+
+    def mark_complete(item_description)
+      if !find_item(item_description).completed?
+        find_item(item_description).done
+    end
+  end
 end
 
 class Item
     # methods and stuff go here
-    attr_reader :description,:completed_status :id
+    attr_reader :description,:id
+    attr_writer :completed_status
 
     # Initilize item with a description
     def initialize(item_description)
       @description = item_description
       @completed_status = false
-      @id = 1
+      @id = rand(100)
     end
 
+    # resturns the completed_status
     def completed?
       @completed_status
+    end
+
+    # marks the items as completed
+    def done
+      @completed_status = true
     end
 
     def get_description
       @description
     end
 
-    def item_id
+    def get_id
       @id
     end
+
 end
--- udacitask.rb
+++ udacitask.rb
@@ -1,7 +1,6 @@
 require_relative 'todolist.rb'
-
 # Creates a new todo list
-my_todo = todolist.new
+my_todo = TodoList.new("Test List")
 # Add four new items
 my_todo.add_item("Task 1")
 my_todo.add_item("Task 2")
@@ -9,18 +8,18 @@ my_todo.add_item("Task 3")
 # Print the list
 my_todo.show
 # Delete the first item
-#my_todo.delete_item()
+my_todo.delete_item("Task 1")
 # Print the list
-#my_todo.show()
+my_todo.show()
 # Delete the second item
-#my_todo.delete_item()
+my_todo.delete_item("Task 2")
 # Print the list
-#my_todo.show()
+my_todo.show()
 # Update the completion status of the first item to complete
-#my_todo.mark_complete()
+my_todo.mark_complete("Task 3")
 # Print the list
-#my_todo.show()
+my_todo.show()
 # Update the title of the list
-#my_todo.set_title()
+my_todo.set_title("My new title")
 # Print the list
-#my_todo.show()
+my_todo.show()
